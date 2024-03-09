4f01c971f9c0e53d009be7e23ebf8ec3cfde8760
”kajol” <kajol3132@gmail.com>
2024-03-09 12:02:24 +0530

    update file

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" { 
+ region = "ap-south-1"
+}
+resource "aws_instance" "mumbai-1" {
+    ami = "ami-03bb6d83c60fc5f7c"
+    instance_type =  "t2.micro"
+    tags = "motogb-1"
+
+    }
