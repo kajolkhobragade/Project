9f748fa70b9c504daef1b0493c10216ad4708ed6
”kajol” <kajol3132@gmail.com>
2024-03-09 18:53:22 +0530

    update

--- main.tf
+++ main.tf
@@ -6,15 +6,19 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   region = "ap-south-1"
+  # Specify your AWS access key ID and secret access key here
+  access_key = "AKIASALHX3NT5K6QZD4Z"
+  secret_key = "JMIl58di1EcsXnwV6pY7DJ0xoOGsvtOPq3egGeqi"
 }
 
 resource "aws_instance" "Mumbai-1" {
   ami           = "ami-03bb6d83c60fc5f7c"
   instance_type = "t2.micro"
-  tags = {
-    "Name" = "server1"
-  }
   
+  tags = {
+    Name = "server1"
+  }
 }
